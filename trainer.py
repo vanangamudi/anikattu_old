60e592b8d833604605b54e5b2978b4f88328b411
M Selvakumar <selva.developer@gmail.com>
2018-04-15 15:45:32 +0530

    saves the best model every epoch EpochAverager to track loss over both in an epoch and across epochs; loss trend works; snapshot only the best model now all outside functions called from trainer.py supplied with feed and batch_index model snapshot filename; in predict returns variable instead of tensor batchops must return tuple of three - indices, inputs, outputs the core of this framework

---
+++ trainer.py
@@ -0,0 +1,261 @@
+import logging
+from config import Config
+from pprint import pprint, pformat
+
+import logging
+from pprint import pprint, pformat
+logging.basicConfig(format="%(levelname)-8s:%(filename)s.%(funcName)20s >>   %(message)s")
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+from debug import memory_consumed
+from utilz import ListTable
+from tqdm import tqdm as _tqdm
+
+import torch
+
+from torch import optim, nn
+
+from collections import namedtuple
+
+Feeder = namedtuple('Feeder', ['train', 'test'])
+
+def tqdm(a):
+    return _tqdm(a) if Config().tqdm else a
+
+class FLAGS:
+    CONTINUE_TRAINING = 0
+    STOP_TRAINING = 1
+
+class Runner(object):
+    def __init__(self, model, *args, **kwargs):
+        self._model = model
+
+    def run(self, input):
+        model_output = self.model(*input)
+        return model_output
+
+    @property
+    def model(self):
+        return self._model
+    
+class Averager(list):
+    def __init__(self, filename=None, *args, **kwargs):
+        super(Averager, self).__init__(*args, **kwargs)
+        if filename:
+            open(filename, 'w').close()
+
+        self.filename = filename
+        
+    @property
+    def avg(self):
+        if len(self):
+            return sum(self)/len(self)
+        else:
+            return 0
+
+
+    def __str__(self):
+        if len(self) > 0:
+            return 'min/max/avg/latest: {:0.5f}/{:0.5f}/{:0.5f}/{:0.5f}'.format(min(self), max(self), self.avg, self[-1])
+        
+        return '<empty>'
+
+    def append(self, a):
+        try:
+            super(Averager, self).append(a.data[0])
+        except:
+            super(Averager, self).append(a)
+            
+    def empty(self):
+        del self[:]
+
+    def write_to_file(self):
+        if self.filename:
+            with open(self.filename, 'a') as f:
+                f.write(self.__str__() + '\n')
+
+class EpochAverager(Averager):
+    def __init__(self, filename=None, *args, **kwargs):
+        super(EpochAverager, self).__init__(*args, **kwargs)
+        self.epoch_cache = Averager(filename, *args, *kwargs)
+
+    def cache(self, a):
+        self.epoch_cache.append(a)
+
+    def clear_cache(self):
+        super(EpochAverager, self).append(self.epoch_cache.avg)
+        self.epoch_cache.empty()
+        
+class Trainer(object):
+    def __init__(self, name, runner=None, model=None,
+                 feeder = None,
+                 optimizer=None,
+                 loss_function = None,
+                 accuracy_function=None,
+                 f1score_function=None,
+                 epochs=10000, checkpoint=1,
+                 directory='results',
+                 *args, **kwargs):
+
+        self.name  = name
+        self.__build_model_group(runner, model, *args, **kwargs)
+        self.__build_feeder(feeder, *args, **kwargs)
+
+        self.epochs     = epochs
+        self.checkpoint = checkpoint
+
+        self.optimizer     = optimizer     if optimizer     else optim.SGD(self.runner.model.parameters(), lr=0.01, momentum=0.1)
+
+        # necessary metrics
+        self.train_loss = EpochAverager(filename = '{}/{}/{}.{}'.format(directory, name, 'metrics',  'train_loss'))
+        self.test_loss  = EpochAverager(filename = '{}/{}/{}.{}'.format(directory, name, 'metrics', 'test_loss'))
+        self.accuracy_function = accuracy_function if accuracy_function else self._default_accuracy_function
+
+        self.accuracy   = EpochAverager(filename = '{}/{}/{}.{}'.format(directory, name, 'metrics', 'accuracy'))
+        self.loss_function = loss_function if loss_function else nn.NLLLoss()
+
+        # optional metrics
+        self.f1score_function = f1score_function
+        self.precision = EpochAverager(filename = '{}/{}/{}.{}'.format(directory, name, 'metrics', 'precision'))
+        self.recall = EpochAverager(filename = '{}/{}/{}.{}'.format(directory, name, 'metrics', 'recall'))
+        self.f1score   = EpochAverager(filename = '{}/{}/{}.{}'.format(directory, name, 'metrics', 'f1score'))
+
+        self.metrics = [self.train_loss, self.test_loss, self.accuracy, self.precision, self.recall, self.f1score]
+        
+        self.best_model = (0, self.runner.model.state_dict())
+        
+
+    def __build_model_group(self, runner, model, *args, **kwargs):
+        assert model is not None or runner is not None, 'both model and runner are None, fatal error'
+        if runner:
+            self.runner = runner
+        else:
+            self.runner = Runner(model)
+            
+    def __build_feeder(self, feeder, *args, **kwargs):
+        assert feeder is not None, 'feeder is None, fatal error'
+        self.feeder = feeder
+
+    def save_best_model(self):
+        log.info('saving the last best model...')
+        torch.save(self.best_model[1], '{}.{}'.format(self.name, 'pth'))
+        
+    def train(self, test_drive=False):
+        self.runner.model.train()
+        for epoch in range(self.epochs):
+            log.critical('memory consumed : {}'.format(memory_consumed()))            
+
+
+            if self.do_every_checkpoint(epoch) == FLAGS.STOP_TRAINING:
+                log.info('loss trend suggests to stop training')
+                return
+
+            for j in tqdm(range(self.feeder.train.num_batch)):
+                log.debug('{}th batch'.format(j))
+                self.optimizer.zero_grad()
+                _, i, t = self.feeder.train.next_batch()
+                output = self.runner.run(i)
+                loss = self.loss_function( output, t, self.feeder.train, j)
+                self.train_loss.append(loss.data[0])
+
+                loss.backward()
+                self.optimizer.step()
+                
+                if test_drive and j >= test_drive:
+                    log.info('-- {} -- loss: {}'.format(epoch, self.train_loss))
+                    return
+            
+            log.info('-- {} -- loss: {}'.format(epoch, self.train_loss))            
+            
+            
+            for m in self.metrics:
+                m.write_to_file()
+                
+        self.runner.model.eval()
+        return True
+        
+    def do_every_checkpoint(self, epoch, early_stopping=True):
+        if epoch % self.checkpoint != 0:
+            return
+        self.runner.model.eval()
+        for j in tqdm(range(self.feeder.test.num_batch)):
+            _, i, t = self.feeder.train.next_batch()
+            output =  self.runner.run(i)
+
+            loss = self.loss_function(output, t, self.feeder.train, j)
+            self.test_loss.cache(loss.data[0])
+            accuracy = self.accuracy_function(output, t, self.feeder.train, j)
+            self.accuracy.cache(accuracy.data[0])
+
+            if self.f1score_function:
+                precision, recall, f1score = self.f1score_function(output, t, self.feeder.train, j)
+                self.precision.append(precision)
+                self.recall.append(recall)
+                self.f1score.append(f1score)
+
+                
+        log.info('-- {} -- loss: {}, accuracy: {}'.format(epoch, self.test_loss.epoch_cache, self.accuracy.epoch_cache))
+        if self.f1score_function:
+            log.info('-- {} -- precision: {}'.format(epoch, self.precision))
+            log.info('-- {} -- recall: {}'.format(epoch, self.recall))
+            log.info('-- {} -- f1score: {}'.format(epoch, self.f1score))
+
+        self.test_loss.clear_cache()
+        self.accuracy.clear_cache()
+        if early_stopping:
+            return self.loss_trend()
+
+        if self.best_model[0] < self.accuracy.avg:
+            self.best_model = (self.accuracy.avg, self.runner.model.state_dict())
+            self.save_best_model()
+
+    def loss_trend(self):
+        if len(self.test_loss) > 4:
+            losses = self.test_loss[-4:]
+            count = 0
+            for l, r in zip(losses, losses[1:]):
+                if l < r:
+                    count += 1
+                    
+            if count > 2:
+                return FLAGS.STOP_TRAINING
+
+        return FLAGS.CONTINUE_TRAINING
+
+
+    def _default_accuracy_function(self):
+        return -1
+
+    
+class Predictor(object):
+    def __init__(self, runner=None, model=None,
+                 feed = None,
+                 repr_function = None,
+                 *args, **kwargs):
+        
+        self.__build_model_group(runner, model, *args, **kwargs)
+        self.__build_feed(feed, *args, **kwargs)
+        self.repr_function = repr_function
+        
+    def __build_model_group(self, runner, model, *args, **kwargs):
+        assert model is not None or runner is not None, 'both model and runner are None, fatal error'
+        if runner:
+            self.runner = runner
+        else:
+            self.runner = Runner(model)
+            
+    def __build_feed(self, feed, *args, **kwargs):
+        assert feed is not None, 'feed is None, fatal error'
+        self.feed = feed
+        
+    def predict(self,  batch_index=0):
+        log.debug('batch_index: {}'.format(batch_index))
+        _, i, *__ = self.feed.nth_batch(batch_index)
+        log.debug('input_shape: {}'.format([j.shape for j in i]))
+        self.runner.model.eval()
+        output = self.runner.run(i)
+        results = ListTable()
+        results.extend( self.repr_function(output, self.feed, batch_index) )
+        output_ = output
+        return output_, results
