9a46341b5e9743b4566cafd9d38a0dc9b17294c6
M Selvakumar <selva.developer@gmail.com>
2018-05-08 03:09:04 +0530

    frequency based filtering; corrects sorting

--- vocab.py
+++ vocab.py
@@ -7,27 +7,48 @@ log.setLevel(logging.INFO)
 
 class Vocab:
 
-    def __init__(self, vocab, special_tokens, sort_key=None):
+    def __init__(self, vocab, special_tokens, max_size=None, sort_key=None, freq_threshold=1):
 
         log.info('Constructiong vocabuluary object...')
         self.vocab = vocab
 
-        index2word = list(vocab.keys())
+        if isinstance(freq_threshold, int):
+            vocab = {w:c for w, c in vocab.items() if c >= freq_threshold}
+        else:
+            l, h = freq_threshold
+            vocab = {w:c for w, c in vocab.items() if c <= l or c >= h}
+
+        vocab = sorted(vocab.items(), key=lambda x: x[1], reverse=True)
+        if max_size: vocab = vocab[:max_size]
+        vocab = [ w for w,c in vocab]
+        index2word = vocab
         if sort_key:
             index2word = sorted(index2word, key=sort_key)
 
-        self.index2word = special_tokens + index2word        
+        self.index2word = special_tokens + index2word
         self.word2index = {w:i for i, w in enumerate(self.index2word)}
 
         log.info('number of word in index2word and word2index: {} and {}'
                  .format(len(self.index2word), len(self.word2index)))
         
     def __getitem__(self, key):
-        if type(key) == int:
+        if type(key) == str:
+            if key in self.word2index:
+                return self.word2index[key]
+            else:
+                return self.word2index['UNK']
+        else: #type(key) == int:
             return self.index2word[key]
-        elif type(key) == str:
-            return self.word2index[key]
-
+        
 
     def __len__(self):
         return len(self.index2word)
+
+    def extend(self, words):        
+        self.word2index.update(
+            {
+                w  :   i + len(self.index2word)
+                for i, w in enumerate(words)
+            })
+        
+        self.index2word += words
