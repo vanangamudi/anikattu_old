ff998f2f543db68b62b16ae9339406f224358384
Mu Selvakumar <selva.developer@gmail.com>
2018-11-19 15:43:18 +0530

    add plots subdir and prefix is used to house the entire directory structure

--- utilz.py
+++ utilz.py
@@ -25,13 +25,16 @@ def mkdir_if_exist_not(name):
     if not os.path.isdir(name):
         return os.mkdir(name)
     
-def initialize_task(hpconfig = 'hpconfig.py'):
+def initialize_task(hpconfig = 'hpconfig.py', prefix='run00'):
     log.info('loading hyperparameters from {}'.format(hpconfig))
-    root_dir = hash_file(hpconfig)[-6:]
+    root_dir = hpconfig.replace('.py', '') + '__' + hash_file(hpconfig)[-6:]
+    mkdir_if_exist_not(prefix)
+    root_dir = '{}/{}'.format(prefix, root_dir)
     mkdir_if_exist_not(root_dir)
     mkdir_if_exist_not('{}/results'.format(root_dir))
     mkdir_if_exist_not('{}/results/metrics'.format(root_dir))
     mkdir_if_exist_not('{}/weights'.format(root_dir))
+    mkdir_if_exist_not('{}/plots'.format(root_dir))
 
     shutil.copy(hpconfig, root_dir)
     shutil.copy('config.py', root_dir)
